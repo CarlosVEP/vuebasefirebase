2b4f5244c7b8391f98a05b93997614cfa0c76eb0
CarlosV <carlos_vep@hotmail.com>
2019-08-05 14:12:29 -0400

    terminado

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <form @submit.prevent="login">
+            <input type="text" v-model="userrr" placeholder="usuario"><br>
+            <input type="password" v-model="passs" placeholder="contraseña"><br>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+    name: 'Login',
+    data(){
+        return{
+            userrr:"",
+            passs: ""
+        }
+    },
+    methods: {
+        login(){
+            firebase.auth().signInWithEmailAndPassword( this.userrr, this.passs).then(user=>{console.log("login ");this.$router.replace('segura')},error=>console.log(error));
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+var firebaseConfig = {
+  apiKey: "AIzaSyDBHipVwtEkYBQ4whANqENyFnIlakY_Hyw",
+  authDomain: "login2-dd17f.firebaseapp.com",
+  databaseURL: "https://login2-dd17f.firebaseio.com",
+  projectId: "login2-dd17f",
+  storageBucket: "",
+  messagingSenderId: "785904655162",
+  appId: "1:785904655162:web:e2e167060cfb4652"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth().onAuthStateChanged(function(user){
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+});
+
